5db545f8fc6ff8e2dddaff1945fd2c739d306d21
André Santos <andreluismagalhaes14@gmail.com>
2024-08-10 20:17:23 -0300

    ALteração para integra com o escritor de arquvo

--- pesquisas.py
+++ pesquisas.py
@@ -1,25 +1,27 @@
 class Pesquisas:
-    def pesquisa_binaria(self, lista, item):
+    @staticmethod
+    def pesquisa_binaria(lista, id):
         baixo = 0
         alto = len(lista) - 1
 
         while baixo <= alto:
             meio = (baixo + alto) // 2
-            chute = lista[meio]
-            if chute.id == item:
+            chute = lista[meio].id
+            if chute == id:
                 return meio
-            if chute.id > item:
+            if chute > id:
                 alto = meio - 1
             else:
                 baixo = meio + 1
-        return None
- 
-    def pesquisa_sequencial(self, lista, item):
+        return -1
+
+    @staticmethod
+    def pesquisa_sequencial(lista, id):
         pos = 0
         found = False
         
         while pos < len(lista) and not found:
-            if lista[pos].id == item:  # Comparando o id do objeto
+            if lista[pos].id == id:  # Comparando o id do objeto
                 found = True
             else:
                 pos = pos + 1
