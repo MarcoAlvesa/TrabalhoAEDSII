0768db8b52e03c83a58c8ab4832fcd6d7549c29b
Marco Antonio <marcoalvesce@gmail.com>
2024-08-11 15:45:10 -0300

    count e BigO

--- merge.py
+++ merge.py
@@ -1,10 +1,15 @@
+import math
+
 class Merge:
     def __init__(self, lista, key=lambda x: x):
         self.lista = lista
         self.key = key
+        self.count = 0
+        self.bigO = len(lista) * math.log(len(lista))
 
     def merge_sort(self, lista):
         if len(lista) > 1:
+            self.count += 1
             # pegando o meio da lista;
             mid = len(lista) // 2
             #Primeira e segunda metade da lista;
@@ -17,6 +22,7 @@ class Merge:
             i = j = k = 0
 
             while i < len(plado) and j < len(slado):
+                self.count += 1
                 if self.key(plado[i]) < self.key(slado[j]):
                     lista[k] = plado[i]
                     i += 1
@@ -27,16 +33,19 @@ class Merge:
 
             # Verificar se há elementos restantes no p_lado
             while i < len(plado):
+                self.count += 1
                 lista[k] = plado[i]
                 i += 1
                 k += 1
 
             # Verificar se há elementos restantes no s_lado
             while j < len(slado):
+                self.count += 1
                 lista[k] = slado[j]
                 j += 1
                 k += 1
 
     def sort(self):
         self.merge_sort(self.lista)
-
+        print(f"Execuções vs Big O: {self.count} vs O({self.bigO})")
+    
--- pesquisas.py
+++ pesquisas.py
@@ -1,14 +1,18 @@
+import math
+
 class Pesquisas:
     @staticmethod
     def pesquisa_binaria(lista, id):
         baixo = 0
         alto = len(lista) - 1
-
+        bigO = math.log(len(lista))
+        count = 0
         while baixo <= alto:
+            count += 1
             meio = (baixo + alto) // 2
             chute = lista[meio].id
             if chute == id:
-                return meio
+                return meio, print(f"Execuções vs Big O: {count} vs O({bigO})")
             if chute > id:
                 alto = meio - 1
             else:
@@ -19,11 +23,14 @@ class Pesquisas:
     def pesquisa_sequencial(lista, id):
         pos = 0
         found = False
-        
+        bigO = len(lista)
+        count = 0
+
         while pos < len(lista) and not found:
+            count += 1
             if lista[pos].id == id:  # Comparando o id do objeto
                 found = True
             else:
                 pos = pos + 1
 
-        return pos if found else None
+        return pos, print(f"Execuções vs Big O: {count} vs O({bigO})") if found else None
