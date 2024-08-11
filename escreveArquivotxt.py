9e2b11e836db9f45bcd772ccb123fb14eea757b4
André Santos <andreluismagalhaes14@gmail.com>
2024-08-11 11:49:33 -0300

    criando pasta para os arquivos

--- escreveArquivotxt.py
+++ escreveArquivotxt.py
@@ -1,11 +1,15 @@
+import  os
+
 class EscreveArquivotxt:
     def __init__(self, nomearq):
         self.nomearq = nomearq
-        with open(nomearq, 'w') as arquivo:
+        os.makedirs("arquivos_txt", exist_ok=True)
+        self.caminhocompleto = os.path.join("arquivos_txt", self.nomearq)
+        with open(self.caminhocompleto, 'w') as arquivo:
             arquivo.write(f'{22 * "="}-ARQUIVO CRIADO-{22 * "="}\n')
 
     def escreveLista(self, lista):
-        with open(self.nomearq, 'a') as arquivo:
+        with open(self.caminhocompleto, 'a') as arquivo:
             for item in lista:
                 # if hasattr(item, 'toString'):
                 arquivo.write(item.__str__() + '\n')
@@ -13,14 +17,14 @@ class EscreveArquivotxt:
 
     def escreveItem(self, lista, indice):
         if indice == -1:
-            with open(self.nomearq, 'a') as arquivo:
+            with open(self.caminhocompleto, 'a') as arquivo:
                 arquivo.write(f'Objeto  não existe!\n')
                 arquivo.write('=' * 60 + '\n')
         else:
-            with open(self.nomearq, 'a') as arquivo:
+            with open(self.caminhocompleto, 'a') as arquivo:
                 arquivo.write(lista[indice].__str__() + '\n')
                 arquivo.write('=' * 60 + '\n')
 
     def escreveTexto(self, texto):
-        with open(self.nomearq, 'a') as arquivo:
+        with open(self.caminhocompleto, 'a') as arquivo:
             arquivo.write(texto)
