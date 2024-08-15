ae8f6219216f19463ce9fdc04cf632444459ed17
André Santos <andreluismagalhaes14@gmail.com>
2024-08-15 16:50:13 -0300

    Testando novos metodos de gerador na classe Autor
    gerador já cria o arquivo e escreve

--- autor.py
+++ autor.py
@@ -1,8 +1,32 @@
+from faker import Faker
+import struct
+import os
+
 class Autor:
-    def __init__(self, id, nome, datanasc):
-        self.id = id
-        self.nome = nome
-        self.datanasc = datanasc
+    def __init__(self):
+        self.id = None
+        self.nome = ''
+        self.datanasc = ''
+        self.fake = Faker()
 
     def __str__(self):
         return f"Autor: [{self.id}] Nome: {self.nome}, Data de nascimento: {self.datanasc}"
+
+    def gerador(self, tamanho):
+        os.makedirs("Bases_dat", exist_ok=True)
+        self.caminhocompleto = os.path.join("Bases_dat", "Autores.dat")
+        with open(self.caminhocompleto, 'wb') as arquivo_binario:
+            for i in range(tamanho):
+                self.id = i + 1
+                self.nome = self.fake.name()
+                self.datanasc = self.fake.date_of_birth(minimum_age=20, maximum_age=90).strftime('%d/%m/%Y')
+                autor = Autor
+
+                idbinario = struct.pack('i', self.id)
+                nome_binario = self.nome.encode('utf-8')
+                datanasc_binario = self.datanasc.encode('utf-8')
+
+                arquivo_binario.write(idbinario)
+                arquivo_binario.write(nome_binario.ljust(50, b'\x00'))
+                arquivo_binario.write(datanasc_binario.ljust(10, b'\x00'))
+        print(f'Base de autores  gerado com sucesso! Tam----> [{tamanho}]')
--- merge.py
+++ merge.py
@@ -1,47 +1,7 @@
-import math
 
 class Merge:
-    def __init__(self, lista, key=lambda x: x):
-        self.lista = lista
-        self.key = key
-        self.count = 0
-        self.bigO = len(lista) * math.log2(len(lista))
+    def __init__(self):
 
-    def merge_sort(self, lista):
-        if len(lista) > 1:
-            # pegando o meio da lista;
-            mid = len(lista) // 2
-            # Primeira e segunda metade da lista;
-            plado = lista[:mid]
-            slado = lista[mid:]
 
-            self.merge_sort(plado)
-            self.merge_sort(slado)
-
-            i = j = k = 0
-
-            while i < len(plado) and j < len(slado):
-                self.count += 1
-                if self.key(plado[i]) < self.key(slado[j]):
-                    lista[k] = plado[i]
-                    i += 1
-                else:
-                    lista[k] = slado[j]
-                    j += 1
-                k += 1
-
-            # Verificar se há elementos restantes no p_lado
-            while i < len(plado):
-                lista[k] = plado[i]
-                i += 1
-                k += 1
-
-            # Verificar se há elementos restantes no s_lado
-            while j < len(slado):
-                lista[k] = slado[j]
-                j += 1
-                k += 1
-
-    def sort(self):
-        self.merge_sort(self.lista)
-    
+    def merge_sort(self, nome_Arquivo):
+        with open(nome_Arquivo,'rb') as arquivo:
